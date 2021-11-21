41894def7db0458cd89d2153deaf39bcfb212d83
adam <adam.kaliszan@gmail.com>
2021-11-21 07:00:59 +0100

    Console description

--- consoleUdp.sh
+++ consoleUdp.sh
@@ -3,7 +3,7 @@ DST_ADDR=${1:-192.168.14.50}
 DST_PORT=${2:-2021}
 
 echo "Usage: $0 [IP addr] [port]"
-echo "Uruchamiam konsolę z komunikacji z modułem pantografowym STM Nucleo H7 ${DST_ADDR} ${DST_PORT}"
+echo "Starting UDP console ${DST_ADDR} ${DST_PORT}. Press enter"
 saved_tty_settings=$(stty -g)
 
 trap "stty $ saved_tty_settings" EXIT
